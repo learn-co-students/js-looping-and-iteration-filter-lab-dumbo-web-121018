f302222daccb2f924d6b7ccf62d2107e48c2f35b
woodylucas <wlucas0311@gmail.com>
2019-01-27 17:11:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 // Code your solution in this file
 
-function findMatching (drivers, name) {
-  // created a function inside filter, that takes an argument of driverName.
-  return drivers.filter(function (driverName) {
-    return driverName.toLowerCase() === name.toLowerCase();
-  });
+
+function findMatching(drivers, name) {
+  return drivers.filter(driver => {
+  return driver.toLowerCase() === name.toLowerCase();
+  })
 }
 
 function fuzzyMatch (drivers, partialName) {
   let lengthOfName = partialName.length;
-  return drivers.filter(function (driverName) {
-    return driverName.slice(0, lengthOfName) === partialName;
-  });
-}
+  return drivers.filter(driver => {
+  return driver.slice(0, lengthOfName) === partialName;
+    })
+  }
+
+
+
 
 function matchName (drivers, name) {
-  return drivers.filter(function (driver) {
-    return driver.name.toLowerCase() === name.toLowerCase();
+  return drivers.filter(driver => {
+  return driver.name.toLowerCase() === name.toLowerCase();
   });
+
 }
